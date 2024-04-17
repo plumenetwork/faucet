d5845797116228d0e1612f8af8dcf012e13faef4
Susie <suujia@icloud.com>
2024-04-17 19:58:41 +0900

    Change background color to gradient red

--- src/app/bitget/page.tsx
+++ src/app/bitget/page.tsx
@@ -5,9 +5,9 @@ import SocialMediaBar from '../components/SocialMediaBar';
 
 export default function Bitget() {
   return (
-    <div className="bg-stone-950 pt-4 px-4 min-h-screen overflow-hidden">
+    <div className="background-red pt-4 px-4 min-h-screen overflow-hidden">
       <Header />
-      <div className="background-image-div gap-4 flex flex-col items-center px-4 pt-8 sm:px-16 sm:pt-20 sm:h-full w-full">
+      <div className="background-image-div background-red gap-4 flex flex-col items-center px-4 pt-8 sm:px-16 sm:pt-20 sm:h-full w-full">
         <CollabHeader />
         <MintBitgetNFT />
         <SocialMediaBar />
