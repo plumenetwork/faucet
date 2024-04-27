e20ce15b41cbe053787e1f2e7e1e2825391c096f
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-04-27 00:53:49 -0400

    [ENG-307] install SVGR and add Discord/Telegram links (#19)

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,29 +1,19 @@
-import alertIcon from './alert-toast.svg';
 import backgroundPattern from './background-pattern.png';
-import bitgetEnded from './bitget-ended.svg';
+import bitgetEnded from './bitget-ended.png';
 import bitgetIcon from './bitget-icon.png';
 import bitgetLogo from './bitget-logo.png';
-import checkmarkIcon from './check-toast.svg';
 import ethereum from './ethereum-icon.png';
 import faucetIcon from './faucet-icon.png';
-import linkedin from './linkedin.png';
 import plume from './plume-logo.png';
 import ringedCheckmark from './ringed-checkmark.png';
-import twitter from './twitter.png';
-import website from './website.png';
 
 export {
-  alertIcon,
   backgroundPattern,
   bitgetEnded,
   bitgetIcon,
   bitgetLogo,
-  checkmarkIcon,
   ethereum,
   faucetIcon,
-  linkedin,
   plume,
-  ringedCheckmark,
-  twitter,
-  website
+  ringedCheckmark
 };
--- src/app/components/SocialMediaBar.tsx
+++ src/app/components/SocialMediaBar.tsx
@@ -1,7 +1,10 @@
-import Image from 'next/image';
 import Link from 'next/link';
 
-import { linkedin, twitter, website } from '../assets';
+import BrandDiscord from '../assets/brand-discord.svg';
+import BrandLinkedin from '../assets/brand-linkedin.svg';
+import BrandTelegram from '../assets/brand-telegram.svg';
+import BrandX from '../assets/brand-x.svg';
+import World from '../assets/world.svg';
 
 const SocialMediaBar = () => (
   <>
@@ -10,13 +13,19 @@ const SocialMediaBar = () => (
     </div>
     <div className="flex gap-3 mt-4 mb-64 max-md:mb-10">
       <Link href="https://www.plumenetwork.xyz">
-        <Image src={website} width={32} height={32} alt="website" />
+        <World className="text-gray-400 hover:text-gray-100" width={32} height={32} />
       </Link>
       <Link href="https://twitter.com/plumenetwork">
-        <Image src={twitter} width={32} height={32} alt="twitter" />
+        <BrandX className="text-gray-400 hover:text-gray-100" width={32} height={32} />
+      </Link>
+      <Link href="https://discord.com/invite/plume">
+        <BrandDiscord className="text-gray-400 hover:text-gray-100" width={32} height={32} />
+      </Link>
+      <Link href="https://t.me/plumenetwork">
+        <BrandTelegram className="text-gray-400 hover:text-gray-100" width={32} height={32} />
       </Link>
       <Link href="https://www.linkedin.com/company/plume-network">
-        <Image src={linkedin} width={32} height={32} alt="linkedin" />
+        <BrandLinkedin className="text-gray-400 hover:text-gray-100" width={32} height={32} />
       </Link>
     </div>
   </>
