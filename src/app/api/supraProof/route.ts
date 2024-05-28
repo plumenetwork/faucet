3dce17a6c9bfde2c8853e9e163ff4479d31d7a0c
re1ro <rma4ok@gmail.com>
2024-05-28 13:29:20 -0400

    feat(OracleGame): getting active pairs from the contact

--- src/app/api/supraProof/route.ts
+++ src/app/api/supraProof/route.ts
@@ -1,10 +1,53 @@
+import {createPublicClient, http} from 'viem'
+import { plumeTestnet } from 'viem/chains'
 import PullServiceClient from './SupraClient'
+import OracleGameAbi from './OracleGameAbi.json'
 
+const web3Client = createPublicClient({
+  chain: plumeTestnet,
+  transport: http()
+})
 const supraClient = new PullServiceClient();
+
 export async function GET(): Promise<Response> {
+  const currentPairIndex = Number(await web3Client.readContract({
+    address: process.env.NEXT_PUBLIC_ORACLE_GAME_CONTRACT_ADDRESS as `0x${string}`,
+    abi: OracleGameAbi.abi,
+    functionName: 'currentPairIndex',
+  }) as BigInt);
+
+  const proofPairs = []
+
+  if (currentPairIndex == -1) {
+    // Game not started yet
+  }
+
+  if (currentPairIndex >= 0) {
+    // Adding current active pair if available
+    const currentPair = Number(await web3Client.readContract({
+      address: process.env.NEXT_PUBLIC_ORACLE_GAME_CONTRACT_ADDRESS as `0x${string}`,
+      abi: OracleGameAbi.abi,
+      functionName: 'allPairs',
+      args: [currentPairIndex]
+    }) as BigInt)
+    proofPairs.push(currentPair);
+  }
+
+  if (currentPairIndex >= 1) {
+    // Adding previous active pair if available
+    const previousPair = Number(await web3Client.readContract({
+      address: process.env.NEXT_PUBLIC_ORACLE_GAME_CONTRACT_ADDRESS as `0x${string}`,
+      abi: OracleGameAbi.abi,
+      functionName: 'allPairs',
+      args: [currentPairIndex - 1]
+    }) as BigInt)
+    proofPairs.push(previousPair);
+  }
+
+  // Getting proof for active pairs from supra
   const proof = await supraClient.getProof({
     // previous and current active pairs in the oracle game contract
-    pairIndexes: [9, 10],
+    pairIndexes: proofPairs,
     chainType: 'evm',
   }).then((response: any) => {
     return response.evm;
