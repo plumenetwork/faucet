eb14f96b2345162d254a82e301f921ce845eac6f
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-06-01 07:35:25 -0500

    Update page styling

--- src/app/components/SocialMediaBar.tsx
+++ src/app/components/SocialMediaBar.tsx
@@ -8,24 +8,18 @@ import World from '../assets/world.svg';
 
 const SocialMediaBar = () => (
   <>
-    <div className="pt-10 text-xs font-medium tracking-wide leading-4 text-center capitalize text-neutral-500">
-      FOLLOW US
+    <div className="pt-4 text-xs font-medium tracking-wide leading-4 text-center uppercase text-gray-600">
+      Follow Us
     </div>
     <div className="flex gap-3 mt-4 mb-64 max-md:mb-10">
       <Link href="https://www.plumenetwork.xyz">
-        <World className="text-gray-400 hover:text-gray-100" width={32} height={32} />
+        <World className="text-gray-500 hover:text-gray-600" width={32} height={32} />
       </Link>
       <Link href="https://twitter.com/plumenetwork">
-        <BrandX className="text-gray-400 hover:text-gray-100" width={32} height={32} />
-      </Link>
-      <Link href="https://discord.com/invite/plume">
-        <BrandDiscord className="text-gray-400 hover:text-gray-100" width={32} height={32} />
-      </Link>
-      <Link href="https://t.me/plumenetwork">
-        <BrandTelegram className="text-gray-400 hover:text-gray-100" width={32} height={32} />
+        <BrandX className="text-gray-500 hover:text-gray-600" width={32} height={32} />
       </Link>
       <Link href="https://www.linkedin.com/company/plume-network">
-        <BrandLinkedin className="text-gray-400 hover:text-gray-100" width={32} height={32} />
+        <BrandLinkedin className="text-gray-500 hover:text-gray-600" width={32} height={32} />
       </Link>
     </div>
   </>
