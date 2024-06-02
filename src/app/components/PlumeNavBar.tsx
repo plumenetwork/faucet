b3192c502ab5ce4bd62efefffa8408b9e5a9c1d4
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-06-01 19:34:16 -0500

    Add testnet connection logic (#31)

--- src/app/components/PlumeNavBar.tsx
+++ src/app/components/PlumeNavBar.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import PlumeLogo from '@/app/components/PlumeLogo';
@@ -5,8 +7,12 @@ import PlumeLogoWithoutText from '@/app/components/PlumeLogoWithoutText';
 import { OpenInNewTabIcon } from '@/app/icons/OpenInNewTabIcon';
 import { config } from '@/app/config';
 import { FC } from 'react';
+import { useAccount } from 'wagmi';
+import { plumeTestnet } from 'wagmi/chains';
 
 const PlumeNavBar: FC = () => {
+  const { isConnected } = useAccount();
+
   return (
     <div className='flex flex-row justify-between bg-white px-8 py-4 text-black'>
       <div className='flex flex-row items-center gap-8 sm:gap-4'>
@@ -28,7 +34,7 @@ const PlumeNavBar: FC = () => {
         </Link>
       </div>
       <div className='flex flex-row gap-4'>
-        <PlumeTestnetIndicator />
+        {isConnected && <PlumeTestnetIndicator />}
         <ConnectButton showBalance={false} />
       </div>
     </div>
@@ -38,11 +44,18 @@ const PlumeNavBar: FC = () => {
 export default PlumeNavBar;
 
 const PlumeTestnetIndicator: FC = () => {
+  const { chainId } = useAccount();
+  const isPlumeTestnet = chainId === plumeTestnet.id;
+
   return (
     <div className='my-auto hidden md:block'>
       <div className='my-auto flex justify-center gap-1 rounded-full border border-solid border-gray-200 bg-gray-50 px-2.5 py-0.5'>
         <div className='my-auto flex flex-col justify-center p-1'>
-          <div className='h-2 w-2 shrink-0 rounded-full bg-green-400' />
+          {isPlumeTestnet ? (
+            <div className='h-2 w-2 shrink-0 rounded-full bg-green-400' />
+          ) : (
+            <div className='h-2 w-2 shrink-0 rounded-full bg-gray-200' />
+          )}
         </div>
         <div className='text-center text-xs font-medium leading-5 text-gray-600'>
           Plume Testnet
