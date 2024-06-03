6a4c7a385d0ffa3813942f59fcd7653efb2513fe
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-06-03 10:51:48 -0400

    datadog test

--- src/app/lib/rateLimiter.ts
+++ src/app/lib/rateLimiter.ts
@@ -20,52 +20,58 @@ export const withRateLimiter =
     limiterKeys: (request: NextRequest) => Promise<string[]>;
     handler: (req: Request) => Promise<Response>;
   }) =>
-  async (request: NextRequest): Promise<Response> => {
-    // Read the request body stream and cache it, because `request.json()` can only be read once.
-    // Caching it allows `limiterKeys` and the route handler to call `await request.json()` later on.
-    const json = await request.json();
-    request.json = () => json;
+    async (request: NextRequest): Promise<Response> => {
+      // Read the request body stream and cache it, because `request.json()` can only be read once.
+      // Caching it allows `limiterKeys` and the route handler to call `await request.json()` later on.
+      const json = await request.json();
+      request.json = () => json;
 
-    if (typeof limiterKeys !== 'function')
-      throw new Error('limiterKeys must be a function');
+      if (typeof limiterKeys !== 'function')
+        throw new Error('limiterKeys must be a function');
 
-    const keys = await limiterKeys(request);
+      const keys = await limiterKeys(request);
 
-    const limits = await Promise.all([
-      ...keys.map((key) =>
-        concurrentRateLimit
-          .limit(`concurrent:${key}`)
-          .then(({ success }) => success)
-      ),
-      ...keys.map((key) =>
-        dailyRateLimit
-          .getRemaining(`daily:${key}`)
-          .then((remaining) => remaining > 0)
-      ),
-    ]);
-
-    const rateLimitUpdates: Promise<any>[] = [];
-
-    try {
-      if (limits.some((success) => !success))
-        return Response.json({ error: 'Rate limit exceeded' }, { status: 429 });
-
-      const response = await handler(request);
-
-      if (response.status === 200) {
-        rateLimitUpdates.push(
-          ...keys.map((key) => dailyRateLimit.limit(`daily:${key}`))
-        );
-      }
-
-      return response;
-    } finally {
-      rateLimitUpdates.push(
+      const limits = await Promise.all([
         ...keys.map((key) =>
-          concurrentRateLimit.resetUsedTokens(`concurrent:${key}`)
-        )
-      );
+          concurrentRateLimit
+            .limit(`concurrent:${key}`)
+            .then(({ success }) => {
+              console.log(key, success);
+              return success;
+            })
+        ),
+        ...keys.map((key) =>
+          dailyRateLimit
+            .getRemaining(`daily:${key}`)
+            .then((remaining) => {
+              console.log(key, remaining);
+              return remaining > 0;
+            })
+        ),
+      ]);
 
-      await Promise.all(rateLimitUpdates);
-    }
-  };
+      const rateLimitUpdates: Promise<any>[] = [];
+
+      try {
+        if (limits.some((success) => !success))
+          return Response.json({ error: 'Rate limit exceeded' }, { status: 429 });
+
+        const response = await handler(request);
+
+        if (response.status === 200) {
+          rateLimitUpdates.push(
+            ...keys.map((key) => dailyRateLimit.limit(`daily:${key}`))
+          );
+        }
+
+        return response;
+      } finally {
+        rateLimitUpdates.push(
+          ...keys.map((key) =>
+            concurrentRateLimit.resetUsedTokens(`concurrent:${key}`)
+          )
+        );
+
+        await Promise.all(rateLimitUpdates);
+      }
+    };
