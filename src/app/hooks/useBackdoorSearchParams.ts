848ec84e0e1d87817c80094b725da8516cdca19a
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-06-08 21:41:58 -0500

    Add backdoor flags

--- src/app/hooks/useBackdoorSearchParams.ts
+++ src/app/hooks/useBackdoorSearchParams.ts
@@ -1,14 +1,23 @@
 import { useSearchParams } from 'next/navigation';
+import { config } from '@/app/config';
 
 export const useBackdoorSearchParams = () => {
   const searchParams = useSearchParams();
 
+  const bypassCloudflareTurnstile =
+    searchParams.get('bypassCloudflareTurnstile') === 'true';
+  const includeMockWallet = searchParams.get('includeMockWallet') === 'true';
+  const mockedWalletAccount = searchParams.get('mockedWalletAccount') as
+    | `0x${string}`
+    | undefined;
+
   return {
-    bypassCloudflareTurnstile:
-      searchParams.get('bypassCloudflareTurnstile') === 'true',
-    includeMockWallet: searchParams.get('includeMockWallet') === 'true',
-    mockedWalletAccount: searchParams.get('mockedWalletAccount') as
-      | `0x${string}`
-      | undefined,
+    bypassCloudflareTurnstile: config.enableBypassCloudflareTurnstile
+      ? bypassCloudflareTurnstile
+      : false,
+    includeMockWallet: config.enabledMockedWallet ? includeMockWallet : false,
+    mockedWalletAccount: config.enabledMockedWalletAccount
+      ? mockedWalletAccount
+      : undefined,
   };
 };
