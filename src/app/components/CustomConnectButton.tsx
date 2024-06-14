943ed2e27f350d7c2c8e19fec7eb43d3a7ca529c
re1ro <rma4ok@gmail.com>
2024-06-14 15:13:08 -0400

    fix(faucet): error message padding

--- src/app/components/CustomConnectButton.tsx
+++ src/app/components/CustomConnectButton.tsx
@@ -88,7 +88,7 @@ export const CustomConnectButton = ({
     return toast({
       title: 'Rate Limit Exceeded',
       description: (
-        <div className='flex flex-row text-sm text-gray-600'>
+        <div className='flex flex-row text-sm text-gray-600 pr-4'>
           Sorry, you can only claim tokens once every 2 hours.
         </div>
       ),
