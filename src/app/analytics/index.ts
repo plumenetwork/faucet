cfb1728c837d54aa0508e6d2a2dcf9acb52f4f73
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-06-17 10:56:09 -0500

    Fix GA events (#60)

--- src/app/analytics/index.ts
+++ src/app/analytics/index.ts
@@ -2,13 +2,21 @@ import { sendGAEvent } from '@next/third-parties/google';
 import { FaucetToken } from '@/app/lib/types';
 
 export const connectWalletButtonClicked = () => {
-  sendGAEvent({ event: 'connectWalletButtonClicked' });
+  sendEvent('connect_wallet_button_clicked');
 };
 
 export const tokenRadioCardSelected = (token: FaucetToken) => {
-  sendGAEvent({ event: 'tokenRadioCardSelected', value: token });
+  sendEvent('token_radio_card_selected', token);
 };
 
 export const getTokensButtonClicked = () => {
-  sendGAEvent({ event: 'getTokensButtonClicked' });
+  sendEvent('get_tokens_button_clicked');
+};
+
+const sendEvent = (eventName: string, eventValue?: Object) => {
+  if (eventValue) {
+    sendGAEvent('event', eventName, eventValue);
+  } else {
+    sendGAEvent('event', eventName);
+  }
 };
