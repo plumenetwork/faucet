b71554a7499fc1a192cdfea917a067f264655766
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-06-29 18:38:59 -0500

    Create metrics endpoint

---
+++ src/app/metrics/route.ts
@@ -0,0 +1,24 @@
+import { Gauge, collectDefaultMetrics, register } from 'prom-client';
+import { config } from '@/app/config';
+import { getAddressBalance } from '@/app/lib/blockchain/utils';
+
+collectDefaultMetrics();
+
+const faucetBalanceGauge = new Gauge({
+  name: 'faucet_balance',
+  help: 'Current balance of the faucet',
+});
+
+export const GET = async (request: Request) => {
+  const faucetBalance = await getAddressBalance(config.faucetContractAddress);
+
+  faucetBalanceGauge.set(faucetBalance);
+
+  const metrics = await register.metrics();
+  return new Response(metrics, {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 'no-store',
+    },
+  });
+};
