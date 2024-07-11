df260bf7476f0544c6a5420c4bfff0674373c7be
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-07-11 16:31:00 -0400

    change "every 10 minutes" to "every 2 hours" (#82)

--- src/app/components/CoreFaucet.tsx
+++ src/app/components/CoreFaucet.tsx
@@ -37,7 +37,7 @@ const CoreFaucet: FC = () => {
             Get Testnet Tokens
           </div>
           <div className='max-w-[280px] font-lufga font-medium sm:max-w-[400px]'>
-            You can get testnet tokens once every 10 minutes to ensure a smooth
+            You can get testnet tokens once every 2 hours to ensure a smooth
             experience for all users.
           </div>
         </div>
