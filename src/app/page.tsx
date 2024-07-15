92d71cf4e168711cf09e4cfa5b41b49b9335f4c6
teaflavored <auster.s.chen@gmail.com>
2024-07-15 04:47:20 -0700

    [ENG-788] Add link back to testnet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link';
 import CoreFaucet from './components/CoreFaucet';
 import PlumeNavBar from './components/PlumeNavBar';
 import SocialMediaBar from './components/SocialMediaBar';
+import { ArrowUpRight } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -8,6 +10,13 @@ export default function Home() {
       <PlumeNavBar />
       <div className='flex w-full flex-col items-center px-4 sm:h-full sm:px-16 sm:pt-20'>
         <CoreFaucet />
+        <Link
+          href='https://miles.plumenetwork.xyz/'
+          target='_blank'
+          className='mt-2 flex items-center gap-1 underline'
+        >
+          Back to Plume Testnet <ArrowUpRight size={18} />
+        </Link>
         <SocialMediaBar />
       </div>
     </div>
