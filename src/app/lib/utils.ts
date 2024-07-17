0aaeffffc0f8c79d901f447a7b251ab152735d4f
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-07-17 05:20:49 -0700

    [TES-90] add CORS headers so we can hit the faucet API from miles.plumenetwork.xyz (#96)

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,6 +1,12 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export const sharedCorsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+};
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
