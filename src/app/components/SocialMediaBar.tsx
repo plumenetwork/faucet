410bb01df71d2addaae4b6623b162a73672eb5cd
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-10-23 13:44:26 -0500

    Remove storybook and clean up spacing

--- src/app/components/SocialMediaBar.tsx
+++ src/app/components/SocialMediaBar.tsx
@@ -11,7 +11,7 @@ const SocialMediaBar = () => (
     <div className='pt-4 text-center text-xs font-medium uppercase leading-4 tracking-wide text-gray-600'>
       Follow Us
     </div>
-    <div className='mb-64 mt-4 flex justify-center gap-3 max-md:mb-10'>
+    <div className='my-4 flex justify-center gap-3 max-md:mb-10'>
       <Link href='https://www.plumenetwork.xyz'>
         <WorldIcon className='text-gray-500 hover:text-gray-600' />
       </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className='min-h-screen overflow-hidden'>
       <PlumeNavBar />
-      <div className='flex w-full flex-col items-center px-4 sm:h-full sm:px-16 sm:pt-20'>
+      <div className='flex w-full flex-col items-center px-4 sm:h-full sm:px-16 sm:py-20'>
         <CoreFaucet />
         <SocialMediaBar />
       </div>
