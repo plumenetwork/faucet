516340771505c863cb48705ecfd4e459d9745c72
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-11-15 13:04:02 -0600

    Customize the main button

--- src/app/components/PlumeNavBar.tsx
+++ src/app/components/PlumeNavBar.tsx
@@ -2,16 +2,10 @@
 
 import Link from 'next/link';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import PlumeLogo from '@/app/components/PlumeLogo';
 import PlumeLogoWithoutText from '@/app/components/PlumeLogoWithoutText';
-import { OpenInNewTabIcon } from '@/app/icons/OpenInNewTabIcon';
-import { config } from '@/app/config';
 import { FC } from 'react';
-import { useFaucetWallet } from '@/app/hooks/useFaucetWallet';
 
 const PlumeNavBar: FC = () => {
-  const { isConnected } = useFaucetWallet();
-
   return (
     <div className='flex flex-row justify-between rounded-3xl bg-white px-8 py-4 text-black'>
       <div className='flex flex-row items-center gap-8 sm:gap-4'>
