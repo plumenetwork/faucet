a34f02a784feeb808378f1b157df2d3f79451e8b
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2024-11-15 13:06:30 -0600

    Update core faucet border color and radius

--- src/app/components/CoreFaucet.tsx
+++ src/app/components/CoreFaucet.tsx
@@ -46,7 +46,7 @@ const CoreFaucet: FC = () => {
   const { isConnected, address } = useFaucetWallet();
 
   return (
-    <div className='flex max-w-full flex-col justify-center gap-4 rounded-2xl border border-solid border-gray-200 bg-neutral-50 bg-white px-5 py-5 md:w-[496px]'>
+    <div className='flex max-w-full flex-col justify-center gap-4 rounded-3xl border border-solid border-[#F0F0F0] bg-white px-5 py-5 md:w-[496px]'>
       <div className='flex flex-col items-center gap-2 text-center'>
         <Image
           alt='Flower icon'
